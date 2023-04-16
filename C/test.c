fa64476b2ebc153c274bf8c4879445677755ef71
HelyusHD <you@example.com>
2023-04-16 20:39:49 +0200

    my first project ever, just some basic stuff

---
+++ C/test.c
@@ -0,0 +1,20 @@
+// how to upload the file to my uni account
+// scp C:\Users\ing-b\Documents\VisualStudioCodeProgramms\LUA\CC\test.c s6anthul@cip-pool.physik.uni-bonn.de:~/Documents/
+
+//compile a c program
+// gcc -Wall -pedantic name.c -o FileName
+
+//execute a file
+// ./FileName
+
+#include <stdio.h> // #include imports lib
+double square(double zahl){
+    return zahl*zahl; // ; is needed to define end of command, in this case the return command
+}
+
+int main(int argc, char **argv){ // char = element from string
+    printf("hello world\n");
+    double a=0.5;
+    printf("Das Quadrat von %f ist %f\n", a, square(a)); // %f is a placeholder and the first cronological %f is replaced with the first following argument
+    return 0;
+}
