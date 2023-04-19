f01e3476e9cf1865aa596b4e949701bb830d6376
HelyusHD <you@example.com>
2023-04-19 11:50:44 +0200

    some more commands (Vorlesung 02)

--- C/test.c
+++ C/test.c
@@ -1,12 +1,13 @@
 // how to upload the file to my uni account
 // scp C:\Users\ing-b\Documents\VisualStudioCodeProgramms\LUA\CC\test.c s6anthul@cip-pool.physik.uni-bonn.de:~/Documents/
 
-
-
-//just a test
+// how to open the manuel:
+// man <function I want to know smth about>
+// man gcc == hekp page related to gcc 
 
 //compile a c program
 // gcc -Wall -pedantic name.c -o FileName
+// add a "-lm" to add a math library
 
 //execute a file
 // ./FileName
@@ -16,7 +17,7 @@ double square(double zahl){
     return zahl*zahl; // ; is needed to define end of command, in this case the return command
 }
 
-int main(int argc, char **argv){ // char = element from string
+int main(int argc, char **argv){ // char = element from string ; argc == count of arguments from the call of this programm, like "./ProgramName FirstInput SecondInput" => argc would = 3
     printf("hello world\n");
     double a=0.5;
     printf("Das Quadrat von %f ist %f\n", a, square(a)); // %f is a placeholder and the first cronological %f is replaced with the first following argument
