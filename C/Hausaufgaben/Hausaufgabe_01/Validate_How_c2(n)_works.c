e7c1216370c8140d5122e4a00844d255bd77276c
HelyusHD <you@example.com>
2023-04-20 20:03:25 +0200

    attempt to better understand a function named "C2"

---
+++ C/Hausaufgaben/Hausaufgabe_01/Validate_How_c2(n)_works.c
@@ -0,0 +1,25 @@
+// We imagine a 2d letice
+// We want to calculate the number of points inside a radius "r" if points are spaced evenly with distance = 1
+
+#include <stdio.h> // #include imports lib
+
+
+    double c2(unsigned int r){
+        for ( unsigned int i =0 ; i<=r ; i++){
+            for ( unsigned int j=0 ; j<=r ; j++){
+                if (i*i + j*j == r){
+                    
+                }
+            }
+        }
+        return 0;
+    }
+
+
+
+
+int main(int argc, char **argv){
+    
+    c2(argv[1]);
+
+}
