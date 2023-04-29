e8783286b00af1cbb63ee9ee217ebcae0db4d1c8
Alican1410 <alicanozcagi@gmail.com>
2023-04-29 13:01:04 +0200

    cubes are confusing

--- C/Hausaufgaben/Hausaufgabe_01/cube.c
+++ C/Hausaufgaben/Hausaufgabe_01/cube.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv){
 
     unsigned int a ;
     a = atof (argv[1]);  // atof is a function which can change from a typ string into an intager typ 
-    fprintf(stderr, "Here we have all points inside a circle with radius %f \n", a);
+    fprintf(stderr, "Here we have all points inside a circle with radius %d \n", a);
     for (int i=0 ; i<=a ; i++){
         fprintf(stderr, "----- cube(%d) -----\n",i);
         cube(i);
@@ -24,17 +24,20 @@ unsigned int anzahl[4]={0,0,0,0}; // anzahl[1]*1, anzahl[2]*1/2, anzahl[3]*1/4, 
 for( int j = -n ; j <= n ; j++ ){   
     for( int k = -n ; k <= n ; k++ ){   
         for( int l = -n ; l <= n ; l++ ){
-            if(j != k && j != l && k != l) anzahl[1] += 1;
-            if((j == k && j != l) || (j == l && j != k) || (k == l && k != j)) anzahl[2] += 1;
-            if((j == k && j == l)) anzahl[3] += 1;
-            if(j == k && j == l) anzahl[4] += 1;
+            if(j != k && j != l && k != l) anzahl[1] += 1; //mittelpunkte
+            if((j == k && j != l) || (j == l && j != k) || (k == l && k != j)) anzahl[2] += 1; // seiten
+            if((j == k && j == l)) anzahl[3] += 1; //kanten
+            if(j == k && j == l){//ecken
+                anzahl[4] += 1;
+                fprintf(stderr, "x and %d \n", anzahl[4]);
+            } 
             }
         }
     }
     fprintf(stderr,"(%d;%d;%d;%d)", anzahl[1], anzahl[2], anzahl[3], anzahl[4]);
 return 0;
 }
-
+// ich möchte mir nochmal die bedingungen besser anschauen, und überlegen warum meine anzahlen sich so komisch verhalten.
 
 
 
