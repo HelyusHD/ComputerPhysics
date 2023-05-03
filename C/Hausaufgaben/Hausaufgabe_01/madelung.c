0c82ab41a01f90ea3f9e6914b4531507c5b22cc4
HelyusHD <you@example.com>
2023-05-03 22:53:16 +0200

    sqaure() funktioniert nicht

--- C/Hausaufgaben/Hausaufgabe_01/madelung.c
+++ C/Hausaufgaben/Hausaufgabe_01/madelung.c
@@ -44,33 +44,37 @@ double cube(int n){
             SurfacePotential += (sign / sqrt(x*x + y*y + z*z));
         }
     }
+
+//attempt to half the runtime by using symmetri of the crystal
+
+/*
+    x = -n;
+    for (int y=-n ; y<=n ; y++){
+        for (int z=-n ; z<=n ; z++){ //not full interval so that edges are not dublicated
+            if (abs(x+y+z)%2 == 0){sign = -1;}else{sign = 1;} // keeping track of the charge of the ions at a lattice point
+            SurfacePotential += (sign / sqrt(x*x + y*y + z*z));
+        }
+    }
+    // face facing in y direction
+    y = -n;
+    for (int x=-n+1 ; x<=n-1 ; x++){
+        for (int z=-n ; z<=n ; z++){
+            if (abs(x+y+z)%2 == 0){sign = -1;}else{sign = 1;}
+            SurfacePotential += (sign / sqrt(x*x + y*y + z*z));
+        }
+    }
+    // face facing in z direction
+    z = -n;
+    for (int x=-n+1 ; x<=n-1 ; x++){
+        for (int y=-n+1 ; y<=n-1 ; y++){
+            if (abs(x+y+z)%2 == 0){sign = -1;}else{sign = 1;}
+            SurfacePotential += (sign / sqrt(x*x + y*y + z*z));
+        }
+    }
+*/
+
     // -x,y,z are missing so we only iterated half the cube
     return 2 * SurfacePotential;
-}
-
-// no inputs required
-int main(int argc, char **argv){ 
-
-    int DecimalPlaces;
-    printf("This program will aproximate the Madelung constant of a NaCl cristal\nPlease specify the exact number of decimal places to be calculated:");
-    scanf("%d", &DecimalPlaces);
-
-    double MadelungConstNaCl = 1.747564; // not full accuracy 
-    double VolumePotential = 0;
-
-    //double accuracy = pow(0.1,DecimalPlaces);
-    double accuracy = 0.00001;
-    int n = 0;
-    while(abs(MadelungConstNaCl-VolumePotential) >= accuracy){
-        n += 1;
-        VolumePotential += cube(n);//sums 
-    }
-    fprintf(stderr,"%f <= %f\n",(abs(MadelungConstNaCl-VolumePotential),accuracy));
-
-    fprintf(stderr,"the total potential sum = %f\nthe final cube turned out to be %d wide.\n", VolumePotential, n*2+1);
-    //fprintf(stderr,"the difference between the calculated and the litrature value is %f\n", abs(MadelungConstNaCl-VolumePotential));
-
-    return 0;
 }
 
 
@@ -92,4 +96,45 @@ double square(int n){
     }
     // -x,y are missing so we only iterated half the cube
     return 2 * SurfacePotential;
+}
+
+
+
+// no inputs required
+int main(int argc, char **argv){ 
+
+    int DecimalPlaces;
+    printf("This program will aproximate the Madelung constant of a NaCl cristal\nPlease specify the exact number of decimal places to be calculated:");
+    scanf("%d", &DecimalPlaces);
+
+    double MadelungConstNaCl = 1.747564; // not full accuracy 
+    double VolumePotential = 0;
+
+    double accuracy = pow(0.1,DecimalPlaces);
+    int n = 0;
+    while((fabs(MadelungConstNaCl - VolumePotential) >= accuracy) && n < 2000){
+        n += 1;
+        VolumePotential += cube(n);//sums all the charges on the cubes surfaces
+        if(n%100 == 0){fprintf(stderr,"iterated %d cubes\ndifference to litrature value = %f\n",n,fabs(MadelungConstNaCl-VolumePotential));}
+    }
+    if (n > 2000){fprintf(stderr,"desired accuracy could not be reached");}
+
+    fprintf(stderr,"\n-----------------\nDONE\n-----------------\n");
+    fprintf(stderr,"the total potential sum = %f\nthe final cube turned out to be %d wide.\n", VolumePotential, n*2+1);
+    fprintf(stderr,"the difference between the calculated and the litrature value is %f\n", fabs(MadelungConstNaCl-VolumePotential));
+
+    fprintf(stderr,"Now we will use a flat cube aka square with the same amount of iterations to calculate the constant.");
+    VolumePotential = 0;
+    int g;
+    for (int m=0 ; m<= n ; m++){
+        VolumePotential += square(m);//sums all the charges potential on the squares circumferences
+        if(n%100 == 0){fprintf(stderr,"iterated %d cubes\ndifference to litrature value = %f\n",n,fabs(MadelungConstNaCl-VolumePotential));}
+        g = m;
+    }
+
+    fprintf(stderr,"\n-----------------\nDONE\n-----------------\n");
+    fprintf(stderr,"the total potential sum = %f\nthe final square turned out to be %d wide.\n", VolumePotential, g*2+1);
+    fprintf(stderr,"the difference between the calculated and the litrature value is %f\n", fabs(MadelungConstNaCl-VolumePotential));
+
+    return 0;
 }
