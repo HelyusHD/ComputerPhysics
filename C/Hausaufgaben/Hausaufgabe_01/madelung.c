38169a24b5958919aa9c693ee2c9700bacccdda2
HelyusHD <you@example.com>
2023-05-03 18:31:28 +0200

    new summing idea for the cube

--- C/Hausaufgaben/Hausaufgabe_01/madelung.c
+++ C/Hausaufgaben/Hausaufgabe_01/madelung.c
@@ -14,61 +14,42 @@
 #include <string.h>
 #include <math.h>
 
-typedef struct {
-    int center;
-    int edge;
-    int face;
-    int corner;
-} CubeCounts; // Type definiert um die Gewichteten Werte zu bestimmten
 
-int* cube(int n, CubeCounts *counts); // Funktion welches jeder Koordinate deren Positionierung zuweist 
-//double sum(CubeCounts[], int unsigned length);
 
-int main(int argc, char **argv){ 
-    CubeCounts counts = {0, 0, 0, 0}; // Counts sind die Anzahl an Stellen, diese werden mit 0 Initialisiert
-    // seitenlaenge sollte von abbruchkrit. abh unsigned int seitenlaenge = atof(argv[1]); // Terminaleingabe der Seitenlaenge des Würfels wird eingelesen
-    int seitenlaenge = //NOCH EINTRAGEN
-    double* weighted = malloc(4*sizeof(int)); // Speicherreservierung für die gewichteten Werte
-    weighted[0] = 0;
-    weighted[1] = 0;
-    weighted[2] = 0;
-    weighted[3] = 0; // gewichtete Werte werden mit 0 initialisiert
-    fprintf(stderr, "Fuer den Wuerfel mit der Seitenlaenge %d finden wir folgende Punkte. \n", seitenlaenge); 
-    for (int i=0 ; i<=seitenlaenge ; i++){ // Schleife die die Funktions cube über i bis eingegebener Seitenlaenge itteriert 
-        cube(i, &counts);// Die Funktion Cube wird mit den Parametern i(aktuelle itterierte Seitenlaenge) aufgerufen und schreibt auf die Adresse von Counts ein.
+double cube(unsigned int n){ // Funktion cube nimmt eine bestimmte Seitenlaenge n und gibt die Anzahl an Mittel-, Seiten-, Kanten- und Eckpunkten zurück
+    double* SurfacePotential = malloc(sizeof(double));
+    SurfacePotential = 0;
+    int* Vector3 = malloc(3 * sizeof(int));
+    Vector3 = {0 , 0 , 0};
+    int* pairing = malloc(9 * sizeof(int));
+    pairing = {1,2,3,2,3,1,3,1,2}; // pairings for x y z which are used to compact the process of summing up points on the cubes surface
+    int sign = 1;
+    for (unsigned int i = 0 ; i<=3 ; i++){
+        Vector3[pairing[i]] = n;
+        if (n%2 == 1){sign = -1;} // keeping track of the charge of the ions at a lattice point
+            for ( Vector3[pairing[i+1]] = 0 ; Vector3[pairing[i+1]] <= n ; Vector3[pairing[i+1]]++){
+                if (Vector3[pairing[i+1]]%2 == 1){sign = -sign;} 
+                for ( Vector3[pairing[i+2]] = 0 ; Vector3[pairing[i+2]] <= n-1 ; Vector3[pairing[i+2]]++){
+                    if (Vector3[pairing[i+2]]%2 == 1){sign = -sign;} 
+                    *SurfacePotential = *SurfacePotential + sign * sqrt(Vector3[1] * Vector3[1] + Vector3[2] * Vector3[2] + Vector3[3] * Vector3[3]);
+                }
+            }
+            if (n%2 == 1){sign = -1;}  else {sign = 1;}
+            *SurfacePotential = 8 * (*SurfacePotential - sign * 2 * sqrt(3 * n * n));
     }
-    weighted[0] = counts.center*1; // Mittelpunkte werden mit dem Faktor 1 gewichtet
-    weighted[1] = counts.edge*(0.5); // Seitenpunkte werden mit dem Faktor 0.5 gewichtet
-    weighted[2] = counts.face*(0.25); // Kantenpunkte werden mit dem Faktor 0.25 gewichtet
-    weighted[3] = counts.corner*(0.125); // Eckpunkte werden mit dem Faktor 0.125 gewichtet
-    printf("Counts: center=%d, edge=%d, face=%d, vertex=%d\n", counts.center, counts.edge, counts.face, counts.corner);
-    printf("weighted: center=%f, edge=%f, face=%f, vertex=%f\n",weighted[0], weighted[1], weighted[2], weighted[3]);
-
-    
-    return 0;
+    return *SurfacePotential;
 }
 
-int* cube(int n, CubeCounts *counts){ // Funktion cube nimmt eine bestimmte Seitenlaenge n und gibt die Anzahl an Mittel-, Seiten-, Kanten- und Eckpunkten zurück
-    for(int x = -n ; x <= n ; x++){   
-        for(int y = -n ; y <= n ; y++){   
-            for(int z = -n ; z <= n ; z++){
-                if (x == 0 && y == 0 && z == 0) {
-                    counts->center++; // Anzahl an Mittelpunkten wird erhöht wenn die Bedingung gilt.
-                } else if ((x == 0 && y == 0 && (z == n || z == -n))
-                           || (x == 0 && z == 0 && (y == n || y == -n))
-                           || (y == 0 && z == 0 && (x == n || x == -n))){
-                    counts->edge++; // Anzahl an Seitenpunkten wird erhöht wenn die Bedingung gilt.
-                } else if ((x == 0 && (y == n || y == -n) && (z == n || z == -n))
-                           || (y == 0 && (x == n || x == -n) && (z == n || z == -n))
-                           || (z == 0 && (x == n || x == -n) && (y == n || y == -n))){
-                    counts->face++; // Anzahl an Kantenpunkten wird erhöht wenn die Bedingung gilt.
-                } else if (x == y && x == z && z == y){
-                    counts->corner++; // Anzahl an Eckpunkten wird erhöht wenn die Bedingung gilt.
-                }
-                //fprintf(stderr, "(%d;%d;%d;%d)\n", counts->center, counts->edge, counts->face, counts->vertex);
-            }
-        }
-    }
+int main(int argc, char **argv){ 
+    unsigned int sidelenght = atof(argv[1]) ;
+    fprintf(stderr, "Fuer den Wuerfel mit der Seitenlaenge %d finden wir folgende Punkte. \n", sidelenght);
+    //double* VolumePotentioal = malloc(sizeof(double));
+    double VolumePotentioal = 0;
+    for (unsigned int n=0 ; n<=sidelenght ; n++){
+        VolumePotentioal = VolumePotentioal + cube(n);//sums 
+
+    fprintf(stderr,"the total potential sum = %f\n", VolumePotentioal);
+
     return 0;
 }
 
