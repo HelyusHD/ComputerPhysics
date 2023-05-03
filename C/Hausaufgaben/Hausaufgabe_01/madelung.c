00121b31ffb2dd62a7e741e8ce10524770c586e2
Alican1410 <alicanozcagi@gmail.com>
2023-05-03 16:19:11 +0200

    work in progress

--- C/Hausaufgaben/Hausaufgabe_01/madelung.c
+++ C/Hausaufgaben/Hausaufgabe_01/madelung.c
@@ -1,3 +1,14 @@
+/********************************************************************************
+ Programm kompelier Befehl:
+ gcc -Wall -pedantic madelung.c -o madelung -lm
+
+ Programm aufruf Befehl:
+ madelung 
+ ********************************************************************************/
+
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,52 +18,52 @@ typedef struct {
     int center;
     int edge;
     int face;
-    int vertex;
-} CubeCounts;
+    int corner;
+} CubeCounts; // Type definiert um die Gewichteten Werte zu bestimmten
 
-int* cube(int n, CubeCounts *counts);
+int* cube(int n, CubeCounts *counts); // Funktion welches jeder Koordinate deren Positionierung zuweist 
 //double sum(CubeCounts[], int unsigned length);
 
-int main(int argc, char **argv){
-    CubeCounts counts = {0, 0, 0, 0};
-    unsigned int a = atof(argv[1]);
-    double* weighted = malloc(4*sizeof(int));
+int main(int argc, char **argv){ 
+    CubeCounts counts = {0, 0, 0, 0}; // Counts sind die Anzahl an Stellen, diese werden mit 0 Initialisiert
+    // seitenlaenge sollte von abbruchkrit. abh unsigned int seitenlaenge = atof(argv[1]); // Terminaleingabe der Seitenlaenge des Würfels wird eingelesen
+    int seitenlaenge = //NOCH EINTRAGEN
+    double* weighted = malloc(4*sizeof(int)); // Speicherreservierung für die gewichteten Werte
     weighted[0] = 0;
     weighted[1] = 0;
     weighted[2] = 0;
-    weighted[3] = 0;
-    fprintf(stderr, "Fuer den Wuerfel mit der Seitenlaenge %d finden wir folgende Punkte. \n", a);
-    for (int i=0 ; i<=a ; i++){
-        //fprintf(stderr, "----- cube(%d) -----\n",i);
-        cube(i, &counts);
+    weighted[3] = 0; // gewichtete Werte werden mit 0 initialisiert
+    fprintf(stderr, "Fuer den Wuerfel mit der Seitenlaenge %d finden wir folgende Punkte. \n", seitenlaenge); 
+    for (int i=0 ; i<=seitenlaenge ; i++){ // Schleife die die Funktions cube über i bis eingegebener Seitenlaenge itteriert 
+        cube(i, &counts);// Die Funktion Cube wird mit den Parametern i(aktuelle itterierte Seitenlaenge) aufgerufen und schreibt auf die Adresse von Counts ein.
     }
-    weighted[0] = counts.center*1;
-    weighted[1] = counts.edge*(0.5);
-    weighted[2] = counts.face*(0.25);
-    weighted[3] = counts.vertex*(0.125);
-    printf("Counts: center=%d, edge=%d, face=%d, vertex=%d\n", counts.center, counts.edge, counts.face, counts.vertex);
+    weighted[0] = counts.center*1; // Mittelpunkte werden mit dem Faktor 1 gewichtet
+    weighted[1] = counts.edge*(0.5); // Seitenpunkte werden mit dem Faktor 0.5 gewichtet
+    weighted[2] = counts.face*(0.25); // Kantenpunkte werden mit dem Faktor 0.25 gewichtet
+    weighted[3] = counts.corner*(0.125); // Eckpunkte werden mit dem Faktor 0.125 gewichtet
+    printf("Counts: center=%d, edge=%d, face=%d, vertex=%d\n", counts.center, counts.edge, counts.face, counts.corner);
     printf("weighted: center=%f, edge=%f, face=%f, vertex=%f\n",weighted[0], weighted[1], weighted[2], weighted[3]);
 
     
     return 0;
 }
 
-int* cube(int n, CubeCounts *counts){
+int* cube(int n, CubeCounts *counts){ // Funktion cube nimmt eine bestimmte Seitenlaenge n und gibt die Anzahl an Mittel-, Seiten-, Kanten- und Eckpunkten zurück
     for(int x = -n ; x <= n ; x++){   
         for(int y = -n ; y <= n ; y++){   
             for(int z = -n ; z <= n ; z++){
                 if (x == 0 && y == 0 && z == 0) {
-                    counts->center++;
+                    counts->center++; // Anzahl an Mittelpunkten wird erhöht wenn die Bedingung gilt.
                 } else if ((x == 0 && y == 0 && (z == n || z == -n))
                            || (x == 0 && z == 0 && (y == n || y == -n))
                            || (y == 0 && z == 0 && (x == n || x == -n))){
-                    counts->edge++;
+                    counts->edge++; // Anzahl an Seitenpunkten wird erhöht wenn die Bedingung gilt.
                 } else if ((x == 0 && (y == n || y == -n) && (z == n || z == -n))
                            || (y == 0 && (x == n || x == -n) && (z == n || z == -n))
                            || (z == 0 && (x == n || x == -n) && (y == n || y == -n))){
-                    counts->face++;
+                    counts->face++; // Anzahl an Kantenpunkten wird erhöht wenn die Bedingung gilt.
                 } else if (x == y && x == z && z == y){
-                    counts->vertex++;
+                    counts->corner++; // Anzahl an Eckpunkten wird erhöht wenn die Bedingung gilt.
                 }
                 //fprintf(stderr, "(%d;%d;%d;%d)\n", counts->center, counts->edge, counts->face, counts->vertex);
             }
@@ -62,11 +73,10 @@ int* cube(int n, CubeCounts *counts){
 }
 
 /* CORRECT EQUATION
-double sum(CubeCounts[], int unsigned length){
+double sum(weight[], int unsigned nstep){
     double sum_value = 0; // this is a temporary variable for iterating, it has to be initialzed to have a starting point
-    
-    MAYBE BETTER IN A WHILE LOOP
-    for(int i = 0 ; i < length ; i++){
+    int unsigned nstep_max = 1000 // einfach ein sehr großer Wert
+    while ( nstep <= int_nstep_max && ( diffrel > int_epsrel ) ){
         sum_value = sum_value + (1/(4*M_PI*	8.8541878128*pow(10,-12)))*((FILL IN*1,602 177 33*pow(10,-16) )/FILL IN);
         if (sum_value) {
             printf("Eine Genauigkeit von 10^-5 wurde erreicht.\n");
@@ -91,4 +101,5 @@ double sum(CubeCounts[], int unsigned length){
 
     while loop 
     while ( nstep <= int_nstep_max && ( diffrel > int_epsrel || diffabs > int_epsabs ) )
+    wir wollen nur den relativen Fehlerbetrachten
 */
